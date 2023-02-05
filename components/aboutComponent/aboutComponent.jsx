f6fa72defe3f920c9f1285739ed05f416cc13845
Zero <56801658+ZeroRex0126@users.noreply.github.com>
2023-02-05 17:18:34 +0200

    button

--- components/aboutComponent/aboutComponent.jsx
+++ components/aboutComponent/aboutComponent.jsx
@@ -8,10 +8,24 @@ const About = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
   .container {
+    .aboutPic {
+      background-size: cover;
+    }
+
     .row {
+      .position-relative {
+        display: block;
+      }
+      // .groom {
+      //   text-align: right;
+      // }
+      // .bride {
+      //   text-align: left;
+      // }
       div {
-        min-height: 40vh;
+        height: 40vh;
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -19,8 +33,27 @@ const About = styled.div`
         padding-right: 0;
         padding-left: 0;
 
-        .aboutPic {
-          background-size: cover;
+        .btn-square {
+          width: 40px;
+          height: 40px;
+        }
+
+        .btn-sm-square {
+          width: 32px;
+          height: 32px;
+        }
+
+        .btn-lg-square {
+          width: 50px;
+          height: 50px;
+        }
+
+        .btn-square,
+        .btn-sm-square,
+        .btn-lg-square {
+          margin-left: 5px;
+          margin-right: 5px;
+          text-align: center;
         }
 
         div {
@@ -35,56 +68,84 @@ const AboutComponent = ({ webSiteSetting }) => {
   return (
     <About id="aboutContainer">
       <h1>About Bride & Groom</h1>
-      <div className="container">
-        <div className="row">
-          {/* py-3 pr-3 pl-3 */}
-          <div className="col-sm-6 col-md-6 col-lg-6 ">
+      <div class="container-fluid py-5" id="about">
+        <div class="container py-5">
+          <div class="section-title position-relative text-center">
+            <h6 class="text-uppercase text-primary mb-3">About</h6>
+            <h1 class="font-secondary display-4">Groom & Bride</h1>
+            <i class="far fa-heart text-dark"></i>
+          </div>
+          <div class="row m-0 mb-4 mb-md-0 pb-2 pb-md-0">
+            <div class="col-md-6 p-0 text-center text-md-right">
+              <div class="h-100 d-flex flex-column justify-content-cente p-5 groom">
+                <h3 class="mb-3">The Groom</h3>
+                <p>
+                  Lorem elitr magna stet rebum dolores sed. Est stet labore est
+                  lorem lorem at amet sea, eos tempor rebum, labore amet ipsum
+                  sea lorem, stet rebum eirmod amet. Kasd clita kasd stet amet
+                  est dolor elitr.
+                </p>
+                <h3 class="font-secondary font-weight-normal text-muted mb-3">
+                  <i class="fa fa-male text-primary pr-3"></i>Jack
+                </h3>
+                <div class="position-relative">
+                  <a class="btn btn-outline-primary btn-square mr-1" href="#">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                  <a class="btn btn-outline-primary btn-square mr-1" href="#">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                  <a class="btn btn-outline-primary btn-square mr-1" href="#">
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                  <a class="btn btn-outline-primary btn-square" href="#">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
             <div
-              className="aboutPic"
+              class="col-md-6 p-0 aboutPic"
               style={{
                 backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
               }}
             ></div>
           </div>
-          <div className="col-sm-6 col-md-6 col-lg-6 ">
-            <div>
-                <h2>Groom</h2>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley of type and scrambled it to make a
-              type specimen book. It has survived not only five centuries, but
-              also the leap into electronic typesetting, remaining essentially
-              unchanged. It was popularised in the 1960s with the release of
-              Letraset sheets containing Lorem Ipsum passages, and more recently
-              with desktop publishing software like Aldus PageMaker including
-              versions of Lorem Ipsum.
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-6 col-md-6 col-lg-6 ">
-            <div>
-                <h2>Bride</h2>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley of type and scrambled it to make a
-              type specimen book. It has survived not only five centuries, but
-              also the leap into electronic typesetting, remaining essentially
-              unchanged. It was popularised in the 1960s with the release of
-              Letraset sheets containing Lorem Ipsum passages, and more recently
-              with desktop publishing software like Aldus PageMaker including
-              versions of Lorem Ipsum.
-            </div>
-          </div>
-          <div className="col-sm-6 col-md-6 col-lg-6 ">
+          <div class="row m-0">
             <div
-              className="aboutPic"
+              class="col-md-6 p-0 aboutPic"
               style={{
                 backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
               }}
             ></div>
+            <div class="col-md-6 p-0 text-center text-md-left">
+              <div class="h-100 d-flex flex-column justify-content-cente p-5 bride">
+                <h3 class="mb-3">The Bride</h3>
+                <p>
+                  Lorem elitr magna stet rebum dolores sed. Est stet labore est
+                  lorem lorem at amet sea, eos tempor rebum, labore amet ipsum
+                  sea lorem, stet rebum eirmod amet. Kasd clita kasd stet amet
+                  est dolor elitr.
+                </p>
+                <h3 class="font-secondary font-weight-normal text-muted mb-3">
+                  <i class="fa fa-female text-primary pr-3"></i>Rose
+                </h3>
+                <div class="position-relative">
+                  <a class="btn btn-outline-primary btn-square mr-1" href="#">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                  <a class="btn btn-outline-primary btn-square mr-1" href="#">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                  <a class="btn btn-outline-primary btn-square mr-1" href="#">
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                  <a class="btn btn-outline-primary btn-square" href="#">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
