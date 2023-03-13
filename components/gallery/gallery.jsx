58c2135b1d6c63dac701e0b16d6e1c69273f0fe5
Zero <56801658+ZeroRex0126@users.noreply.github.com>
2023-03-13 13:44:21 +0200

    slider complete

--- components/gallery/gallery.jsx
+++ components/gallery/gallery.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import useEmblaCarousel from "embla-carousel-react";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { flushSync } from "react-dom";
 
 const GalleryComp = styled.div`
   background-size: cover;
@@ -27,7 +28,7 @@ const GalleryComp = styled.div`
   }
   .embla__slide {
     height: 260px;
-    width: 250px;
+    width: 310px;
     flex: 0 0 var(--slide-size);
     min-width: 0;
     // padding-left: var(--slide-spacing);
@@ -39,145 +40,70 @@ const GalleryComp = styled.div`
     }
   }}
 `;
+const TWEEN_FACTOR = 4.2;
+
+const numberWithinRange = (number, min, max) =>
+  Math.min(Math.max(number, min), max);
 
 const Gallery = (props) => {
-  const { slides, options } = props;
-  const [emblaRef] = useEmblaCarousel(options);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const [tweenValues, setTweenValues] = useState([]);
 
+  const onScroll = useCallback(() => {
+    if (!emblaApi) return;
+
+    const engine = emblaApi.internalEngine();
+    const scrollProgress = emblaApi.scrollProgress();
+
+    const styles = emblaApi.scrollSnapList().map((scrollSnap, index) => {
+      if (!emblaApi.slidesInView().includes(index)) return 0;
+      let diffToTarget = scrollSnap - scrollProgress;
+
+      if (engine.options.loop) {
+        engine.slideLooper.loopPoints.forEach((loopItem) => {
+          const target = loopItem.target().get();
+          if (index === loopItem.index && target !== 0) {
+            const sign = Math.sign(target);
+            if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress);
+            if (sign === 1) diffToTarget = scrollSnap + (1 - scrollProgress);
+          }
+        });
+      }
+      const tweenValue = 1 - Math.abs(diffToTarget * TWEEN_FACTOR);
+      return numberWithinRange(tweenValue, 0, 1);
+    });
+    setTweenValues(styles);
+  }, [emblaApi, setTweenValues]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onScroll();
+    emblaApi.on("scroll", () => {
+      flushSync(() => onScroll());
+    });
+    emblaApi.on("reInit", onScroll);
+  }, [emblaApi, onScroll]);
+
+  let array = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
   return (
     <GalleryComp id="gallery">
       <h1>this is gallery</h1>
       <div className="embla">
         <div className="embla__viewport" ref={emblaRef}>
           <div className="embla__container">
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
-            <div className="embla__slide">
-              <div
-                className="img-fluid mr-md-3 slideImg"
-                style={{
-                  backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
-                }}
-              ></div>
-            </div>
+            {array.map((i) => {
+              return (
+                <div className="embla__slide" key={i}>
+                  <div
+                    className="img-fluid mr-md-3 slideImg"
+                    style={{
+                      backgroundImage: `url(data:image/jpeg;base64,${props.webSiteSetting.heroimg})`,
+                    }}
+                  ></div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
