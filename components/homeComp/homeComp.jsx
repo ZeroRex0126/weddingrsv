9d57cbfbb290e5cd4a089f050d1068836223824f
Zero <56801658+ZeroRex0126@users.noreply.github.com>
2023-03-14 15:58:34 +0200

    fix home style

--- components/homeComp/homeComp.jsx
+++ components/homeComp/homeComp.jsx
@@ -1,30 +1,49 @@
 import TimerCard from "../timerCard/timerCard";
+import styled from "styled-components";
 
+const HomeCom = styled.div`
+  .main {
+    background-size: cover;
+    min-height: 100vh;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .timer {
+      max-width: 100vw;
+      display: flex;
+    }
+  }
+`;
 const HomeComp = ({ webSiteSetting, remainingTime }) => {
   return (
-    <div
-      className="main"
-      style={{
-        backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
-      }}
-      id="home"
-    >
-      <div>
-        <h1>{webSiteSetting.title}</h1>
+    <HomeCom>
+      <div
+        className="main"
+        style={{
+          backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
+        }}
+        id="home"
+      >
+        <div>
+          <h1>{webSiteSetting.title}</h1>
+        </div>
+        <div className="timer">
+          {remainingTime.years > 0 ? (
+            <TimerCard title="years" time={remainingTime.years} />
+          ) : (
+            <></>
+          )}
+          <TimerCard title="months" time={remainingTime.months} />
+          <TimerCard title="days" time={remainingTime.days} />
+          <TimerCard title="hours" time={remainingTime.hours} />
+          <TimerCard title="minutes" time={remainingTime.minutes} />
+          <TimerCard title="seconds" time={remainingTime.seconds} />
+        </div>
       </div>
-      <div className="timer">
-        {remainingTime.years > 0 ? (
-          <TimerCard title="years" time={remainingTime.years} />
-        ) : (
-          <></>
-        )}
-        <TimerCard title="months" time={remainingTime.months} />
-        <TimerCard title="days" time={remainingTime.days} />
-        <TimerCard title="hours" time={remainingTime.hours} />
-        <TimerCard title="minutes" time={remainingTime.minutes} />
-        <TimerCard title="seconds" time={remainingTime.seconds} />
-      </div>
-    </div>
+    </HomeCom>
   );
 };
 
