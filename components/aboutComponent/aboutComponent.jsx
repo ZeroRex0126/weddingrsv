45fbea67187c4a3004313d5f154777aee73b7e12
Zero <56801658+ZeroRex0126@users.noreply.github.com>
2023-03-16 12:20:18 +0200

    next to each other

--- components/aboutComponent/aboutComponent.jsx
+++ components/aboutComponent/aboutComponent.jsx
@@ -19,12 +19,6 @@ const About = styled.div`
       .position-relative {
         display: block;
       }
-      // .groom {
-      //   text-align: right;
-      // }
-      // .bride {
-      //   text-align: left;
-      // }
       div {
         // height: 40vh;
         display: flex;
@@ -73,74 +67,94 @@ const AboutComponent = ({ webSiteSetting }) => {
           <h1 className="font-secondary display-4">Groom & Bride</h1>
           <i className="far fa-heart text-dark"></i>
         </div>
-        <div className="row m-0 mb-4 mb-md-0 pb-2 pb-md-0">
-          <div className="col-md-6 p-0 text-center text-md-right">
-            <div className="h-100 d-flex flex-column justify-content-cente p-5 groom">
-              <h3 className="mb-3">The Groom</h3>
-              <p>
-                Lorem elitr magna stet rebum dolores sed. Est stet labore est
-                lorem lorem at amet sea, eos tempor rebum, labore amet ipsum sea
-                lorem, stet rebum eirmod amet. Kasd clita kasd stet amet est
-                dolor elitr.
-              </p>
-              <h3 className="font-secondary font-weight-normal text-muted mb-3">
-                <i className="fa fa-male text-primary pr-3"></i>Jack
-              </h3>
-              <div className="position-relative">
-                <a className="btn btn-outline-primary btn-square mr-1" href="#">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a className="btn btn-outline-primary btn-square mr-1" href="#">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a className="btn btn-outline-primary btn-square mr-1" href="#">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-                <a className="btn btn-outline-primary btn-square" href="#">
-                  <i className="fab fa-instagram"></i>
-                </a>
+        <div className="row m-0">
+          <div className="col-md-6 p-0">
+            <div
+              className="aboutPic"
+              style={{
+                backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
+              }}
+            ></div>
+            <div className="text-center">
+              <div className="h-100 d-flex flex-column justify-content-cente p-5 groom">
+                <h3 className="mb-3">The Groom</h3>
+                <p>
+                  Lorem elitr magna stet rebum dolores sed. Est stet labore est
+                  lorem lorem at amet sea, eos tempor rebum, labore amet ipsum
+                  sea lorem, stet rebum eirmod amet. Kasd clita kasd stet amet
+                  est dolor elitr.
+                </p>
+                <h3 className="font-secondary font-weight-normal text-muted mb-3">
+                  <i className="fa fa-male text-primary pr-3"></i>Jack
+                </h3>
+                <div className="position-relative">
+                  <a
+                    className="btn btn-outline-primary btn-square mr-1"
+                    href="#"
+                  >
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a
+                    className="btn btn-outline-primary btn-square mr-1"
+                    href="#"
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a
+                    className="btn btn-outline-primary btn-square mr-1"
+                    href="#"
+                  >
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                  <a className="btn btn-outline-primary btn-square" href="#">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <div
-            className="col-md-6 p-0 aboutPic"
-            style={{
-              backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
-            }}
-          ></div>
-        </div>
-        <div className="row m-0">
-          <div
-            className="col-md-6 p-0 aboutPic"
-            style={{
-              backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
-            }}
-          ></div>
-          <div className="col-md-6 p-0 text-center text-md-left">
-            <div className="h-100 d-flex flex-column justify-content-cente p-5 bride">
-              <h3 className="mb-3">The Bride</h3>
-              <p>
-                Lorem elitr magna stet rebum dolores sed. Est stet labore est
-                lorem lorem at amet sea, eos tempor rebum, labore amet ipsum sea
-                lorem, stet rebum eirmod amet. Kasd clita kasd stet amet est
-                dolor elitr.
-              </p>
-              <h3 className="font-secondary font-weight-normal text-muted mb-3">
-                <i className="fa fa-female text-primary pr-3"></i>Rose
-              </h3>
-              <div className="position-relative">
-                <a className="btn btn-outline-primary btn-square mr-1" href="#">
-                  <i className="fab fa-instagram"></i>
-                </a>
-                <a className="btn btn-outline-primary btn-square mr-1" href="#">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a className="btn btn-outline-primary btn-square mr-1" href="#">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-                <a className="btn btn-outline-primary btn-square" href="#">
-                  <i className="fab fa-instagram"></i>
-                </a>
+          <div className="col-md-6 p-0">
+            <div
+              className="aboutPic"
+              style={{
+                backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
+              }}
+            ></div>
+            <div className="text-center">
+              <div className="h-100 d-flex flex-column justify-content-cente p-5 bride">
+                <h3 className="mb-3">The Bride</h3>
+                <p>
+                  Lorem elitr magna stet rebum dolores sed. Est stet labore est
+                  lorem lorem at amet sea, eos tempor rebum, labore amet ipsum
+                  sea lorem, stet rebum eirmod amet. Kasd clita kasd stet amet
+                  est dolor elitr.
+                </p>
+                <h3 className="font-secondary font-weight-normal text-muted mb-3">
+                  <i className="fa fa-female text-primary pr-3"></i>Rose
+                </h3>
+                <div className="position-relative">
+                  <a
+                    className="btn btn-outline-primary btn-square mr-1"
+                    href="#"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                  <a
+                    className="btn btn-outline-primary btn-square mr-1"
+                    href="#"
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a
+                    className="btn btn-outline-primary btn-square mr-1"
+                    href="#"
+                  >
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                  <a className="btn btn-outline-primary btn-square" href="#">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
