c7a25b6de3e98f814904af7c19f3440fe76f29b4
Zero <56801658+ZeroRex0126@users.noreply.github.com>
2023-03-17 13:52:21 +0200

    added reveal

--- components/aboutComponent/aboutComponent.jsx
+++ components/aboutComponent/aboutComponent.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styled from "styled-components";
 import { BsInstagram } from "react-icons/bs";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { Fade } from "react-awesome-reveal";
 
 const About = styled.div`
   background-size: cover;
@@ -69,94 +70,98 @@ const AboutComponent = ({ webSiteSetting }) => {
         </div>
         <div className="row m-0">
           <div className="col-md-6 p-4">
-            <div
-              className="aboutPic"
-              style={{
-                backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
-              }}
-            ></div>
-            <div className="text-center">
-              <div className="h-100 d-flex flex-column justify-content-cente p-5 groom">
-                <h3 className="mb-3">The Groom</h3>
-                <p>
-                  Lorem elitr magna stet rebum dolores sed. Est stet labore est
-                  lorem lorem at amet sea, eos tempor rebum, labore amet ipsum
-                  sea lorem, stet rebum eirmod amet. Kasd clita kasd stet amet
-                  est dolor elitr.
-                </p>
-                <h3 className="font-secondary font-weight-normal text-muted mb-3">
-                  <i className="fa fa-male text-primary pr-3"></i>Jack
-                </h3>
-                <div className="position-relative">
-                  <a
-                    className="btn btn-outline-primary btn-square mr-1"
-                    href="#"
-                  >
-                    <FaTwitter />
-                  </a>
-                  <a
-                    className="btn btn-outline-primary btn-square mr-1"
-                    href="#"
-                  >
-                    <FaFacebookF />
-                  </a>
-                  {/* <a
+            <Fade direction="left" duration={2000}>
+              <div
+                className="aboutPic"
+                style={{
+                  backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
+                }}
+              ></div>
+              <div className="text-center">
+                <div className="h-100 d-flex flex-column justify-content-cente p-5 groom">
+                  <h3 className="mb-3">The Groom</h3>
+                  <p>
+                    Lorem elitr magna stet rebum dolores sed. Est stet labore
+                    est lorem lorem at amet sea, eos tempor rebum, labore amet
+                    ipsum sea lorem, stet rebum eirmod amet. Kasd clita kasd
+                    stet amet est dolor elitr.
+                  </p>
+                  <h3 className="font-secondary font-weight-normal text-muted mb-3">
+                    <i className="fa fa-male text-primary pr-3"></i>Jack
+                  </h3>
+                  <div className="position-relative">
+                    <a
+                      className="btn btn-outline-primary btn-square mr-1"
+                      href="#"
+                    >
+                      <FaTwitter />
+                    </a>
+                    <a
+                      className="btn btn-outline-primary btn-square mr-1"
+                      href="#"
+                    >
+                      <FaFacebookF />
+                    </a>
+                    {/* <a
                     className="btn btn-outline-primary btn-square mr-1"
                     href="#"
                   >
                     <i className="fab fa-linkedin-in"></i>
                   </a> */}
-                  <a className="btn btn-outline-primary btn-square" href="#">
-                    <BsInstagram />
-                  </a>
+                    <a className="btn btn-outline-primary btn-square" href="#">
+                      <BsInstagram />
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Fade>
           </div>
           <div className="col-md-6 p-4">
-            <div
-              className="aboutPic"
-              style={{
-                backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
-              }}
-            ></div>
-            <div className="text-center">
-              <div className="h-100 d-flex flex-column justify-content-cente p-5 bride">
-                <h3 className="mb-3">The Bride</h3>
-                <p>
-                  Lorem elitr magna stet rebum dolores sed. Est stet labore est
-                  lorem lorem at amet sea, eos tempor rebum, labore amet ipsum
-                  sea lorem, stet rebum eirmod amet. Kasd clita kasd stet amet
-                  est dolor elitr.
-                </p>
-                <h3 className="font-secondary font-weight-normal text-muted mb-3">
-                  <i className="fa fa-female text-primary pr-3"></i>Rose
-                </h3>
-                <div className="position-relative">
-                  <a
-                    className="btn btn-outline-primary btn-square mr-1"
-                    href="#"
-                  >
-                    <FaTwitter />
-                  </a>
-                  <a
-                    className="btn btn-outline-primary btn-square mr-1"
-                    href="#"
-                  >
-                    <FaFacebookF />
-                  </a>
-                  {/* <a
+            <Fade direction="right" duration={2000}>
+              <div
+                className="aboutPic"
+                style={{
+                  backgroundImage: `url(data:image/jpeg;base64,${webSiteSetting.heroimg})`,
+                }}
+              ></div>
+              <div className="text-center">
+                <div className="h-100 d-flex flex-column justify-content-cente p-5 bride">
+                  <h3 className="mb-3">The Bride</h3>
+                  <p>
+                    Lorem elitr magna stet rebum dolores sed. Est stet labore
+                    est lorem lorem at amet sea, eos tempor rebum, labore amet
+                    ipsum sea lorem, stet rebum eirmod amet. Kasd clita kasd
+                    stet amet est dolor elitr.
+                  </p>
+                  <h3 className="font-secondary font-weight-normal text-muted mb-3">
+                    <i className="fa fa-female text-primary pr-3"></i>Rose
+                  </h3>
+                  <div className="position-relative">
+                    <a
+                      className="btn btn-outline-primary btn-square mr-1"
+                      href="#"
+                    >
+                      <FaTwitter />
+                    </a>
+                    <a
+                      className="btn btn-outline-primary btn-square mr-1"
+                      href="#"
+                    >
+                      <FaFacebookF />
+                    </a>
+                    {/* <a
                     className="btn btn-outline-primary btn-square mr-1"
                     href="#"
                   >
                     <i className="fab fa-linkedin-in"></i>
                   </a> */}
-                  <a className="btn btn-outline-primary btn-square" href="#">
-                    <BsInstagram />
-                  </a>
+                    <a className="btn btn-outline-primary btn-square" href="#">
+                      <BsInstagram />
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Fade>
           </div>
         </div>
       </div>
