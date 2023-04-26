20cefe0ec1dad9a74c50c02d3b899b8d2b4844f6
Zero <56801658+ZeroRex0126@users.noreply.github.com>
2023-04-26 14:07:21 +0200

    get data from mongo DB

--- pages/api/webdata.js
+++ pages/api/webdata.js
@@ -1,9 +1,38 @@
 import getDatas from "./datas";
 
+import { MongoClient, ObjectId } from "mongodb";
+
+const connectionString = process.env.DB_STRING;
+let client;
+
 async function handler(req, res) {
+  try {
+    client = await MongoClient.connect(connectionString);
+  } catch (error) {
+    res.status(500).json({ message: "Could not connect to database." });
+    return;
+  }
+
+  const db = client.db();
   switch (req.method) {
     case "GET":
-      res.json(getDatas());
+      // res.json(getDatas());
+      // break;
+      try {
+        let result = await db.collection("websiteData").find({}).toArray();
+        console.log("here", result);
+        if (result) {
+          res.json(result);
+        } else {
+          res.status(500).json({ message: "Storing message failed!" });
+          return;
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Storing message failed!" });
+        return;
+      } finally {
+        client.close();
+      }
       break;
     default:
       break;
